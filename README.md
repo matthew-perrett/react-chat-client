177b6b6cb15279b2e32b004d39f2d8f6b95f8579
mperrett <mperrett@aconex.com>
2020-01-25 14:22:29 -0800

    WIP

---
+++ README.md
@@ -0,0 +1,1 @@
+https://codeutopia.net/blog/2016/01/25/getting-started-with-npm-and-browserify-in-a-react-project/
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const ReactDOM = require('react-dom');
+const React = require('react');
+const Chat = require('./Chat');
+
+ReactDOM.render(
+	  <Chat />,
+	  document.getElementById('app')
+);
