52fd0c90a8fc91795e4914473c96da4c5036e51d
mperrett <mperrett@aconex.com>
2020-01-26 11:15:26 -0800

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 https://codeutopia.net/blog/2016/01/25/getting-started-with-npm-and-browserify-in-a-react-project/
+
+
+
+????
+Lastly, we return an empty object. The function we’re exporting returns an object representing the host connection. This means we could have multiple connections if we want. This is also why we’re directly exporting the function rather than an object. Exporting an object is convenient for things we’ll always have only one of, like the message store, but sometimes we return constructor functions like this. Exported React components also behave in a somewhat similar way.
--- src/HostConnection.js
+++ src/HostConnection.js
@@ -1,17 +1,38 @@
-const SimplePeer = require('simple-peer');
-const SimpleWebsocket = require('simple-websocket');
+const Peer = require('simple-peer');
+const Websocket = require('simple-websocket');
 const EventEmitter = require('events').EventEmitter;
 
 const peers = [];
 const emitter = new EventEmitter();
 
 module.exports = function() {
-    const socket = new SimpleWebsocket('ws://localhost:3210');
+    const socket = new Websocket('ws://localhost:3210');
     socket.on('close', function() { console.log('Socket closed'); });
     socket.on('error', function(err) { console.log('Socket error'); console.log(err); });
     socket.on('connect', function() { console.log('Connected'); });
 
     socket.on('data', function(data) {
+        const rtc = new Peer({ initiator: false, trickle: false });
+
+        rtc.signal(data);
+
+        rtc.on('signal', function(data) {
+            socket.send(data);
+        });
+
+        rtc.on('connect', function() {
+            peers.push(rtc);
+        });
+
+        rtc.on('data', function(msg) {
+            emitter.emit('message', msg);
+            //as host, we need to broadcast the data to the other peers
+            peers.forEach(function(p) {
+                if(p !== rtc) {
+                    p.send(msg);
+                }
+            });
+        });
 
     });
 
